567a5750effad51d0236b71213feaebb0c24d792
arrowstraight <37030740+arrowstraight@users.noreply.github.com>
2019-04-19 10:09:54 +0100

    new files added

---
+++ src/sum.js
@@ -0,0 +1,5 @@
+//jest test
+function sum(a ,b) {
+    return a+b;
+}
+module.exports = sum;
---
+++ tests/sum.test.js
@@ -0,0 +1,8 @@
+//sum test jest file
+const sum = require('../src');
+descride("sum", () => {
+
+})
+it('adds 1 + 2 to equal 3', () => 
+
+)};
